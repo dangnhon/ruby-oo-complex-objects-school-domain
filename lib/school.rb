af402b2314b820d4d9180b52e209715dea4f614f
dangnhon <dang.nhon12345@yahoo.com>
2020-11-18 20:38:38 -0800

    commit tree 65206266028da7404d00241fac14212a92b76c68

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,44 @@
 # code here!
+require 'pry'
+
+class School 
+
+    attr_accessor :roster
+    def initialize(name)
+        @name = name 
+        @roster = {} 
+    end 
+
+    def name 
+        @name
+    end  
+
+
+    def add_student(student, grade)
+        @roster[grade] ||= [] 
+        @roster[grade] << student
+    end 
+
+    def grade(grade_number)
+        roster.each do |key, value| 
+            if key ==  grade_number
+              
+               return value
+            end 
+        end 
+
+        # if grade == @roster[grade]
+        #     @roster[grade]
+        #end
+    end 
+
+    def sort
+        roster.each do |grade, student|
+            roster[grade] = student.sort
+        end 
+        roster
+    end 
+  
+
+
+end 
